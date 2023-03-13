49a5aee32977933713777026696818c208a0054e
candedrums <127731864+candedrums@users.noreply.github.com>
2023-03-13 10:22:54 +0100

    Update productor.c

--- productor.c
+++ productor.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/proc.h>
 
 #define N 10 //número de elementos del buffer
+#define iteraciones 100
 
 
 int cuenta = 0; //numero de elementos del búfer en cada momento
@@ -12,13 +16,13 @@ int tope = -1; //tope de la pila
 
 
 //función para producir un elemento
-int producir_elemento() {
+int produce_item() {
 	return rand() % 100; //genera un número aleatorio entre 0 y 99
 }
 
 
 //función para insertar un elemento en el buffer
-void insertar_elemento(int elemento) {
+void insert_item(int elemento) {
 	tope++;
 	buffer[tope] = elemento;
 }
@@ -26,13 +30,16 @@ void insertar_elemento(int elemento) {
 //función del productor
 void productor(void) {
 	int elemento;
+	int i = 0;
 	
-	while(1) {
-		elemento = producir_elemento();
+	while(i < iteraciones) {
+		elemento = produce_item();
 		if(cuenta==N) sleep();
 		//se despierta e inserta (el consumidor ha quitado un elemento del buffer)
-		insertar_elemento(elemento);
+		insert_item(elemento);
+		printf("[Productor]: Se ha producido el elemento %d y se ha introducido en el buffer\n", elemento);
 		cuenta+=1;
 		if(cuenta==1) wakeup(consumidor);
+		i++;
 	}
 }
