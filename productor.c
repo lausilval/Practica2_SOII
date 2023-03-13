280ee1cd3bcf24983ad16a63877bc9da70b60acf
candedrums <127731864+candedrums@users.noreply.github.com>
2023-03-13 09:59:14 +0100

    Update productor.c

--- productor.c
+++ productor.c
@@ -7,6 +7,21 @@
 
 
 int cuenta = 0;
+int buffer[N];
+int tope = -1;
+
+
+//función para producir un elemento
+int producir_elemento() {
+	return rand() % 100;
+}
+
+
+//función para insertar un elemento en el buffer
+void insertar_elemento(int elemento) {
+	tope++;
+	pila[tope] = elemento;
+}
 
 //función del productor
 void productor(void) {
@@ -20,6 +35,3 @@ void productor(void) {
 		if(cuenta==1) wakeup(consumidor);
 	}
 }
-
-
-
